c03b70b1ee1ecb0b1a1c3d253c7fc109b0613f44
Eduardopatriota <39865404+Eduardopatriota@users.noreply.github.com>
2020-01-23 17:51:41 -0300

    versão funcionando 1
    a versão está totalmente funcional
    dependendo do relógio elamostra uma foto troca o fundo e exibe a hora.
    pretendo colocar uma mensagem de bom dia boa tarde e boa noite  no futuro

---
+++ app.js
@@ -0,0 +1,32 @@
+
+function carregar (){
+
+let msg = window.document.getElementById('msg')
+let img = window.document.getElementById('imagem')
+let data = new Date()
+let hora = data.getHours()
+let min = data.getMinutes()
+var mensagem =''
+
+
+msg.innerHTML = `Agora são ${hora} horas e ${min} min.
+<br> ${mensagem}`
+
+
+    if (hora >= 5 && hora < 12){
+        img.src = 'img/foto_manha.png'
+        mensagem = 'bom dia'
+    }
+    else if (hora >= 12 && hora < 18){
+        img.src = 'img/foto_tarde.png'
+        document.body.style.background = 'orange'
+        mensagem = 'boa tarde'
+    }
+    else{
+        img.src = 'img/foto_noite.png'
+        document.body.style.background = 'gray'
+        mensagem = 'boa noite'
+    }
+
+
+}
